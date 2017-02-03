9cd43ed97e7395a739a780ebdd2d14e9b7b7d890
Daniel <lucha0075.db@gmail.com>
2017-02-03 12:33:54 +0000

    Added Score class

---
+++ Score.java
@@ -0,0 +1,45 @@
+
+public class Score {
+	
+	private int score = 0;
+	private int streak = 0;
+	private int streakGauge = 0;
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+	/**
+	 * @return the streak
+	 */
+	public int getStreak() {
+		return streak;
+	}
+	/**
+	 * @param streak the streak to set
+	 */
+	public void setStreak(int streak) {
+		this.streak = streak;
+	}
+	/**
+	 * @return the streakGauge
+	 */
+	public int getStreakGauge() {
+		return streakGauge;
+	}
+	/**
+	 * @param streakGauge the streakGauge to set
+	 */
+	public void setStreakGauge(int streakGauge) {
+		this.streakGauge = streakGauge;
+	}
+	
+
+}
