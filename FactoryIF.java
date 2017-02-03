0db3657fc52720d0907408f5bc66e0ab5e06b6ee
Daniel <lucha0075.db@gmail.com>
2017-02-03 18:04:10 +0000

    removed comments not needed from FactoryIF

--- FactoryIF.java
+++ FactoryIF.java
@@ -1,18 +1,10 @@
-
 /**
  * interface for object number
- * @author danbro
- *
  */
 interface FactoryIF
 {
 	/**
 	 * Method to create number ParentNumber objects 
-	 * @param amount = type of object
-	 * @param x 
-	 * @param y
-	 * @return
 	 */
 	public ParentNumber createNumber(int amount, int x, int y);
 }
-
